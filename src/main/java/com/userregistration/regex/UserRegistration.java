8d1ab039f1bfc66d7f2f10c45969775f0251221b
Kunal <kunalsonawane730@gmail.com>
2022-11-28 17:26:38 +0530

    adding program of regex pattern to enter last name from user

--- src/main/java/com/userregistration/regex/UserRegistration.java
+++ src/main/java/com/userregistration/regex/UserRegistration.java
@@ -10,6 +10,8 @@ public class UserRegistration {
         // Method To Check First Name With Regex Start With Cap And 3 Min. Character
         System.out.println("Enter the first Name: ");
         String firstName = scan.nextLine();
+        System.out.println("Enter the last Name :");
+        String lastName = scan.nextLine();
 
         if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
             System.out.println("First name is valid");
@@ -17,6 +19,11 @@ public class UserRegistration {
             System.out.println("Entered first name is invalid");
         }
         scan.close();
+	// Method To Check Last Name With Regex Start With Cap And 3 Min. Character
+    if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+        System.out.println("Last name is valid");
+    } else {
+        System.out.println("Entered last name is invalid");
+    }
 	}
-
 }
